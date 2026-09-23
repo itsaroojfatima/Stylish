1918fb30a9120a82d4fed84fa823649132a79851
Arooj Fatima <aroojfatimapk388@gmail.com>
2026-09-23 13:50:49 +0500

    Create index.tsx

---
+++ src/app/index.tsx
@@ -0,0 +1,37 @@
+import { useRouter } from "expo-router";
+import { useEffect } from "react";
+import { Image, StyleSheet, View } from "react-native";
+
+export default function Index() {
+  const router = useRouter();
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.replace("/");
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, [router]);
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require("../../assets/images/splash.png")}
+        style={styles.image}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    width: "100%",
+    height: 100,
+    resizeMode: "contain",
+  },
+});
