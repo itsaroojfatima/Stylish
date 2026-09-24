655d8671919976ff1107f407b19169e259633856
Arooj Fatima <aroojfatimapk388@gmail.com>
2026-09-24 13:25:56 +0500

    Update index.tsx

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,7 +6,7 @@ export default function Index() {
   const router = useRouter();
   useEffect(() => {
     const timer = setTimeout(() => {
-      router.replace("/");
+      router.push("/onboarding");
     }, 2000);
 
     return () => clearTimeout(timer);
